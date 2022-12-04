860cfb0b6c1b61999e87c0038b722fefdc26784d
Marcel <marcel@schuerch.id>
2022-12-04 15:16:13 +0100

    refactored solution of day 1 part 1

--- src/me/gwydi/aoc/day1/Calories.java
+++ src/me/gwydi/aoc/day1/Calories.java
@@ -3,18 +3,22 @@ package me.gwydi.aoc.day1;
 import me.gwydi.aoc.ChallengeInputReader;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Calories {
     public static void main(String[] args) {
         String input = new ChallengeInputReader("src/me/gwydi/aoc/day1/input.txt").readAsString();
-        System.out.println(getHighestCalories(input));
+        System.out.println(getHighestCalories(parse(input)));
     }
 
-    static int getHighestCalories(String input) {
-        var result = Arrays.stream(input.split("\n\n"))
+    static List<Integer> parse(String input) {
+        return Arrays.stream(input.split("\n\n"))
                 .map(s -> Arrays.stream(s.split("\n"))
-                        .map(Integer::parseInt).reduce(Integer::sum).orElse(0))
-                .max(Integer::compare);
+                        .map(Integer::parseInt).reduce(Integer::sum).orElse(0)).toList();
+    }
+
+    static int getHighestCalories(List<Integer> input) {
+        var result = input.stream().max(Integer::compare);
         if (result.isPresent()) {
             return result.get();
         }
